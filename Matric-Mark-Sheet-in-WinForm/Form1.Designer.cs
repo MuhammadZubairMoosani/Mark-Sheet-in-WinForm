eb2bf4bd26d8ba4d3b5b14c017c349b8df37c46b
Muhammad Zubair <muhammadzubairmoosani@gmail.com>
2019-04-22 14:16:18 -0700

    first commit

---
+++ Matric-Mark-Sheet-in-WinForm/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace Metric_Mark_Sheet_in_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.rollNoBox = new System.Windows.Forms.TextBox();
+            this.engSubBox = new System.Windows.Forms.TextBox();
+            this.mathSubBox = new System.Windows.Forms.TextBox();
+            this.hisSubBox = new System.Windows.Forms.TextBox();
+            this.geoSubBox = new System.Windows.Forms.TextBox();
+            this.totalBox = new System.Windows.Forms.TextBox();
+            this.averageBox = new System.Windows.Forms.TextBox();
+            this.gradeBox = new System.Windows.Forms.TextBox();
+            this.totalBtn = new System.Windows.Forms.Button();
+            this.averageBtn = new System.Windows.Forms.Button();
+            this.gradeBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 61);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Roll No.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 107);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "English";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(24, 153);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Mathematic";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(24, 199);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "History";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(24, 245);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Geography";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(24, 291);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Total";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(24, 337);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(68, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Average";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(24, 383);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(63, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Greade";
+            // 
+            // nameBox
+            // 
+            this.nameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameBox.Location = new System.Drawing.Point(124, 13);
+            this.nameBox.Margin = new System.Windows.Forms.Padding(4);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(170, 22);
+            this.nameBox.TabIndex = 9;
+            this.nameBox.TextChanged += new System.EventHandler(this.nameBox_TextChanged);
+            // 
+            // rollNoBox
+            // 
+            this.rollNoBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rollNoBox.Location = new System.Drawing.Point(124, 59);
+            this.rollNoBox.Margin = new System.Windows.Forms.Padding(4);
+            this.rollNoBox.Name = "rollNoBox";
+            this.rollNoBox.Size = new System.Drawing.Size(170, 22);
+            this.rollNoBox.TabIndex = 10;
+            // 
+            // engSubBox
+            // 
+            this.engSubBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.engSubBox.Location = new System.Drawing.Point(124, 105);
+            this.engSubBox.Margin = new System.Windows.Forms.Padding(4);
+            this.engSubBox.Name = "engSubBox";
+            this.engSubBox.Size = new System.Drawing.Size(170, 22);
+            this.engSubBox.TabIndex = 11;
+            this.engSubBox.TextChanged += new System.EventHandler(this.engSubBox_TextChanged);
+            // 
+            // mathSubBox
+            // 
+            this.mathSubBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mathSubBox.Location = new System.Drawing.Point(124, 151);
+            this.mathSubBox.Margin = new System.Windows.Forms.Padding(4);
+            this.mathSubBox.Name = "mathSubBox";
+            this.mathSubBox.Size = new System.Drawing.Size(170, 22);
+            this.mathSubBox.TabIndex = 12;
+            // 
+            // hisSubBox
+            // 
+            this.hisSubBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hisSubBox.Location = new System.Drawing.Point(124, 197);
+            this.hisSubBox.Margin = new System.Windows.Forms.Padding(4);
+            this.hisSubBox.Name = "hisSubBox";
+            this.hisSubBox.Size = new System.Drawing.Size(170, 22);
+            this.hisSubBox.TabIndex = 13;
+            // 
+            // geoSubBox
+            // 
+            this.geoSubBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.geoSubBox.Location = new System.Drawing.Point(124, 243);
+            this.geoSubBox.Margin = new System.Windows.Forms.Padding(4);
+            this.geoSubBox.Name = "geoSubBox";
+            this.geoSubBox.Size = new System.Drawing.Size(170, 22);
+            this.geoSubBox.TabIndex = 14;
+            // 
+            // totalBox
+            // 
+            this.totalBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalBox.Location = new System.Drawing.Point(124, 289);
+            this.totalBox.Margin = new System.Windows.Forms.Padding(4);
+            this.totalBox.Name = "totalBox";
+            this.totalBox.Size = new System.Drawing.Size(170, 22);
+            this.totalBox.TabIndex = 15;
+            // 
+            // averageBox
+            // 
+            this.averageBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.averageBox.Location = new System.Drawing.Point(124, 335);
+            this.averageBox.Margin = new System.Windows.Forms.Padding(4);
+            this.averageBox.Name = "averageBox";
+            this.averageBox.Size = new System.Drawing.Size(170, 22);
+            this.averageBox.TabIndex = 16;
+            // 
+            // gradeBox
+            // 
+            this.gradeBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gradeBox.Location = new System.Drawing.Point(124, 381);
+            this.gradeBox.Margin = new System.Windows.Forms.Padding(4);
+            this.gradeBox.Name = "gradeBox";
+            this.gradeBox.Size = new System.Drawing.Size(170, 22);
+            this.gradeBox.TabIndex = 17;
+            // 
+            // totalBtn
+            // 
+            this.totalBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalBtn.Location = new System.Drawing.Point(331, 92);
+            this.totalBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.totalBtn.Name = "totalBtn";
+            this.totalBtn.Size = new System.Drawing.Size(112, 42);
+            this.totalBtn.TabIndex = 18;
+            this.totalBtn.Text = "TOTAL";
+            this.totalBtn.UseVisualStyleBackColor = true;
+            this.totalBtn.Click += new System.EventHandler(this.totalBtn_Click);
+            // 
+            // averageBtn
+            // 
+            this.averageBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.averageBtn.Location = new System.Drawing.Point(331, 160);
+            this.averageBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.averageBtn.Name = "averageBtn";
+            this.averageBtn.Size = new System.Drawing.Size(112, 42);
+            this.averageBtn.TabIndex = 19;
+            this.averageBtn.Text = "AVERAGE";
+            this.averageBtn.UseVisualStyleBackColor = true;
+            this.averageBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // gradeBtn
+            // 
+            this.gradeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gradeBtn.Location = new System.Drawing.Point(331, 226);
+            this.gradeBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.gradeBtn.Name = "gradeBtn";
+            this.gradeBtn.Size = new System.Drawing.Size(112, 42);
+            this.gradeBtn.TabIndex = 20;
+            this.gradeBtn.Text = "GRADE";
+            this.gradeBtn.UseVisualStyleBackColor = true;
+            this.gradeBtn.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearBtn.Location = new System.Drawing.Point(331, 294);
+            this.clearBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(112, 42);
+            this.clearBtn.TabIndex = 21;
+            this.clearBtn.Text = "CLEAR";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitBtn.Location = new System.Drawing.Point(331, 361);
+            this.exitBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(112, 42);
+            this.exitBtn.TabIndex = 22;
+            this.exitBtn.Text = "EXIT";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 432);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.gradeBtn);
+            this.Controls.Add(this.averageBtn);
+            this.Controls.Add(this.totalBtn);
+            this.Controls.Add(this.gradeBox);
+            this.Controls.Add(this.averageBox);
+            this.Controls.Add(this.totalBox);
+            this.Controls.Add(this.geoSubBox);
+            this.Controls.Add(this.hisSubBox);
+            this.Controls.Add(this.mathSubBox);
+            this.Controls.Add(this.engSubBox);
+            this.Controls.Add(this.rollNoBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox rollNoBox;
+        private System.Windows.Forms.TextBox engSubBox;
+        private System.Windows.Forms.TextBox mathSubBox;
+        private System.Windows.Forms.TextBox hisSubBox;
+        private System.Windows.Forms.TextBox geoSubBox;
+        private System.Windows.Forms.TextBox totalBox;
+        private System.Windows.Forms.TextBox averageBox;
+        private System.Windows.Forms.TextBox gradeBox;
+        private System.Windows.Forms.Button totalBtn;
+        private System.Windows.Forms.Button averageBtn;
+        private System.Windows.Forms.Button gradeBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button exitBtn;
+    }
+}
+
---
+++ Matric-Mark-Sheet-in-WinForm/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Metric_Mark_Sheet_in_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nameBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void engSubBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        double english, math, history, geography, subTotal, average;
+
+        private void totalBtn_Click(object sender, EventArgs e)
+        {
+            english = Convert.ToDouble(engSubBox.Text);
+            math = Convert.ToDouble(mathSubBox.Text);
+            history = Convert.ToDouble(hisSubBox.Text);
+            geography = Convert.ToDouble(geoSubBox.Text);
+            subTotal = english + math + history + geography;
+            string grandTotal = Convert.ToString(subTotal);
+            totalBox.Text = grandTotal;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            average = subTotal / 4;
+            string avrgeInStr = Convert.ToString(average);
+            averageBox.Text = avrgeInStr;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (average >= 95)
+            {
+                gradeBox.Text = "A+";
+            }
+            else if (average >= 90)
+            {
+                gradeBox.Text = "A";
+            }
+            else if (average >= 80)
+            {
+                gradeBox.Text = "B";
+            }
+            else if (average >= 70)
+            {
+                gradeBox.Text = "C";
+            }
+            else if (average >= 60)
+            {
+                gradeBox.Text = "D";
+            }
+            else
+            {
+                gradeBox.Text = "Fail";
+            }
+        }
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            nameBox.Text = "";
+            rollNoBox.Text = "";
+            engSubBox.Text = "";
+            mathSubBox.Text = "";
+            hisSubBox.Text = "";
+            geoSubBox.Text = "";
+            totalBox.Text = "";
+            averageBox.Text = "";
+            gradeBox.Text = "";
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
